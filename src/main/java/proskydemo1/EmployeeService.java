31f980c08a0dcaf09b5a3ef5d4f284cc8e53d3a9
New <Dar1985@bk.ru>
2024-07-25 00:27:17 +0700

    Init

---
+++ src/main/java/proskydemo1/EmployeeService.java
@@ -0,0 +1,12 @@
+
+package proskydemo1;
+import proskydemo1.Demo1Application;
+public interface EmployeeService {
+     Employee add(String firstName, String lastName);
+
+     Employee remove(String firstName, String lastName);
+
+     Employee find(String firstName, String lastName);
+
+     collection<Employee> findAll();
+}
